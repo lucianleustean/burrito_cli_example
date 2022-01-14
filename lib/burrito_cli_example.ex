55e5f59c9e718cb49b265ac8cf3428bf1eb6c229
Lucian Leustean <lucian.leustean@gmail.com>
2022-01-14 18:35:16 +0200

    Add borrito to the mix with a simple example

--- lib/burrito_cli_example.ex
+++ lib/burrito_cli_example.ex
@@ -15,4 +15,18 @@ defmodule BurritoCliExample do
   def hello do
     :world
   end
+
+  @doc """
+  Burrito cli entry point.
+  """
+  def start(_, _args) do
+    IO.puts "#=> Application started !"
+
+    args = Burrito.Util.Args.get_arguments()
+    IO.puts("#=> Passed arguments: #{inspect(args)}")
+
+    # TODO: Make a http request
+
+    System.halt(0)
+  end
 end

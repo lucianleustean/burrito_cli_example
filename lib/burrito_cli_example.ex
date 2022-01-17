9d2e1ffb3ec4960132561dc373b5c31c588f28d2
Lucian Leustean <lucian.leustean@gmail.com>
2022-01-17 21:48:57 +0200

    Add Tesla http client and make a request for a random quote

--- lib/burrito_cli_example.ex
+++ lib/burrito_cli_example.ex
@@ -2,6 +2,11 @@ defmodule BurritoCliExample do
   @moduledoc """
   Documentation for `BurritoCliExample`.
   """
+  use Tesla
+
+  plug(Tesla.Middleware.BaseUrl, "http://quotes.stormconsultancy.co.uk/")
+  plug(Tesla.Middleware.Headers, [{"accept", "application/json"}])
+  plug(Tesla.Middleware.JSON)
 
   @doc """
   Hello world.
@@ -20,12 +25,19 @@ defmodule BurritoCliExample do
   Burrito cli entry point.
   """
   def start(_, _args) do
-    IO.puts "#=> Application started !"
+    IO.puts("#=> Application started !")
 
     args = Burrito.Util.Args.get_arguments()
     IO.puts("#=> Passed arguments: #{inspect(args)}")
 
-    # TODO: Make a http request
+    IO.puts("#=> GET http://quotes.stormconsultancy.co.uk/random")
+
+    get("/random")
+    |> case do
+      {:ok, response} -> response.body
+      {:error, error} -> error
+    end
+    |> IO.inspect()
 
     System.halt(0)
   end
--- mix.exs
+++ mix.exs
@@ -25,7 +25,9 @@ defmodule BurritoCliExample.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:burrito, github: "burrito-elixir/burrito"}
+      {:burrito, github: "burrito-elixir/burrito"},
+      {:jason, ">= 1.0.0"},
+      {:tesla, "~> 1.4.4"}
     ]
   end
 
